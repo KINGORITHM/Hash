6dfeb82ab800e628f30de119a5b40a53cc82f3dd
Choiyunh_ <svs101@naver.com>
2019-10-13 16:42:37 +0900

    19/10/13 solved bestAlbum.cpp from Programmers_Hash_4

--- uno/pg_hash_4.cpp
+++ uno/pg_hash_4.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <unordered_map>
 #include <algorithm>
-#include <functional>
 
 using namespace std;
 unordered_map<string, vector<pair<int, int>>> um;
@@ -15,21 +14,37 @@ bool compare(pair<int, int> a, pair<int, int> b){
 		return a.first > b.first;  // 재생횟수 내림차순	
 }
 
+bool cmp(pair<int, vector<int>> a, pair<int, vector<int>> b) {
+	return a.first > b.first;
+}
+
 vector<int> solution(vector<string> genres, vector<int> plays) {
 	vector<int> answer;
-	
-	for (int i = 0; i < genres.size(); i++)
-		um[genres[i]].push_back(pair<int, int>(plays[i], i));
+
+	for (int i = 0; i < genres.size(); i++) 
+		um[genres[i]].push_back(pair<int, int>(plays[i], i));		
 
 	unordered_map<string, vector<pair<int, int>>>::iterator it = um.begin();
-
+	vector<pair<int, vector<int>>> v(um.size()); 
+	// vector<pair<int, vector<int>>> v; 벡터의 사이즈 지정 매우 중 요 이 것 때 문 에 런 타 임 *****
+	int i = 0;
 	for (it; it != um.end(); it++) {
 		vector<pair<int, int>> temp = it->second;
-		sort(temp.begin(), temp.end(), compare);
-		for (int i = 0; i < it->second.size(); i++) {
-			printf("%d\n", temp[i].second);
-		}
 		
+		sort(temp.begin(), temp.end(), compare);
+		for (int j = 0; j < temp.size(); j++) {
+			v[i].first += temp[j].first;
+			v[i].second.push_back(temp[j].second);
+		}	
+		i++;
+	}
+
+	sort(v.begin(), v.end(), cmp);
+	for (int i = 0; i < v.size(); i++) {
+		for (int j = 0; j < v[i].second.size() && j < 2; j++) {
+			answer.push_back(v[i].second[j]);
+			//printf("%d\n", v[i].second[j]);
+		}		
 	}
 	
 	return answer;
